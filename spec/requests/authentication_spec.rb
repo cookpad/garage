69eceebc34ee92a199195d32db5cab44e4cf80dd
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-05-06 17:41:02 -0700

    refactor spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -6,21 +6,31 @@ describe Garage do
   let(:token) { client_is_authorized(application, user) }
 
   describe 'HTTP request' do
+    before do
+      with_access_token_header token_value if token_value
+      get '/echo'
+    end
+
+    subject { status }
+
     context 'without any token' do
+      let(:token_value) { nil }
       it 'returns 401' do
-        get '/echo'
-        status.should == 401
+        subject.should == 401
+      end
+    end
+
+    context 'with bad token' do
+      let(:token_value) { nil }
+      it 'returns 401' do
+        subject.should == 401
       end
     end
 
     context 'with an authorized token' do
-      before do
-        with_access_token_header token.token
-      end
-
+      let(:token_value) { token.token }
       it 'returns 200' do
-        get '/echo'
-        status.should == 200
+        subject.should == 200
       end
     end
   end
