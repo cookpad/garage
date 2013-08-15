c57cfeb0e9019921470dad3a9286963f811f8d4e
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-08-14 21:28:39 -0700

    Make error more visible

--- lib/garage/exceptions.rb
+++ lib/garage/exceptions.rb
@@ -7,7 +7,7 @@ module Garage
     def initialize(user, action, resource_class, status = :forbidden, scopes = [])
       @status = status
       if scopes.empty?
-        super "Authorized user is not allowed to take the requested operation"
+        super "Authorized user is not allowed to take the requested operation #{action} on #{resource_class}"
       else
         super "Insufficient scope to process the requested operation. Missing scope(s): #{scopes.join(", ")}"
       end
