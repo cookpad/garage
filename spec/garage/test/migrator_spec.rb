528aad60e94b0abb9c19a0326a3d230ed8777d50
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-10-10 17:55:06 +0900

    Restore tables after testing migrator for other tests

--- spec/garage/test/migrator_spec.rb
+++ spec/garage/test/migrator_spec.rb
@@ -4,6 +4,10 @@ require "garage/test/migrator"
 describe Garage::Test::Migrator do
   include described_class
 
+  after(:all) do
+    CreateDoorkeeperTables.migrate(:up)
+  end
+
   it "creates tables for doorkeeper" do
     Doorkeeper::Application.table_exists?.should == true
     Doorkeeper::AccessGrant.table_exists?.should == true
