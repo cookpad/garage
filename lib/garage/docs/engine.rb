50531db832455092e15ce016862965423623075d
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-11-10 19:38:01 -0800

    Move engine routes to initializer, to avoid NameError in the config/routes without :require in Gemfile. Fix #71
      ...bundler/gems/garage-25c84afe4bc4/config/routes.rb:18:in `<top (required)>': uninitialized constant Garage::Webhook::Engine (NameError)

--- lib/garage/docs/engine.rb
+++ lib/garage/docs/engine.rb
@@ -10,6 +10,20 @@ module Garage
   module Docs
     class Engine < ::Rails::Engine
       isolate_namespace Garage::Docs
+
+      initializer "garage.docs.engine.routes" do
+        Engine.routes.append do
+          root :to => 'resources#index', as: nil
+          resources :resources do
+            collection do
+              get 'console'
+              post 'authenticate'
+              get 'callback'
+              post 'callback'
+            end
+          end
+        end
+      end
     end
   end
 end
--- lib/garage/meta/engine.rb
+++ lib/garage/meta/engine.rb
@@ -4,6 +4,13 @@ module Garage
   module Meta
     class Engine < ::Rails::Engine
       isolate_namespace Garage::Meta
+
+      initializer "garage.meta.engine.routes" do
+        Engine.routes.append do
+          resources :services
+          resources :docs
+        end
+      end
     end
   end
 end
