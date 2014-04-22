2d01ec298ac35ffae93e610a0bd91d88b7fc717a
Yuichi Tateno <hotchpotch@gmail.com>
2014-04-22 22:40:38 +0900

    Add document title in docs

--- app/controllers/garage/docs/resources_controller.rb
+++ app/controllers/garage/docs/resources_controller.rb
@@ -17,6 +17,7 @@ class Garage::Docs::ResourcesController < Garage::ApplicationController
   def show
     @documents = Garage::Docs::Document.all
     @examples = Garage::Docs::Example.build_all(self, @document.examples(_current_user))
+    @title = "#{@document.name.humanize} API"
   end
 
   def console
