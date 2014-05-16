dbc5ccd23e6e08af89a8cf7f45f2455f8b329cff
Taiki ONO <taiki-ono@cookpad.com>
2014-05-16 10:04:06 +0900

    Extract util methods to Utils

--- lib/garage/controller_helper.rb
+++ lib/garage/controller_helper.rb
@@ -1,6 +1,8 @@
 module Garage
   module ControllerHelper
     extend ActiveSupport::Concern
+    include Utils
+
     included do
       use Rack::AcceptDefault
 
@@ -69,31 +71,6 @@ module Garage
 
     def ability_from_token
       Garage::TokenScope.ability(current_resource_owner, doorkeeper_token.try(:scopes) || [])
-    end
-
-    # TODO move this to ::Utils
-
-    # Private: extract date time range query from query parameters
-    # Treat `from` and `to` as aliases for `gte` and `lte` respectively
-    def extract_datetime_query(prefix)
-      query = {}
-      {:from => :gte, :to => :lte, :gt => nil, :lt => nil, :gte => nil, :lte => nil}.each do |key, as|
-        k = "#{prefix}.#{key}"
-        if params.has_key?(k)
-          query[as || key] = fuzzy_parse(params[k]) or raise Garage::BadRequest, "Can't parse datetime #{params[k]}"
-        end
-      end
-      query if query.size > 0
-    end
-
-    def fuzzy_parse(date)
-      if date.is_a?(Numeric) || /^\d+$/ === date
-        Time.zone.at(date.to_i)
-      else
-        Time.zone.parse(date)
-      end
-    rescue ArgumentError
-      nil
     end
 
     def notify_request_stats
