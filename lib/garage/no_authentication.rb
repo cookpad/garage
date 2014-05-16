5751c7c862d7d555d035e9c6655c785ad646dc99
Taiki ONO <taiki-ono@cookpad.com>
2014-05-16 10:04:06 +0900

    Add documents for no_authentication

--- lib/garage/no_authentication.rb
+++ lib/garage/no_authentication.rb
@@ -1,5 +1,7 @@
+# Public: Garage controller helper for non-authentication usage.
+# Include this helper after RestfulActions so that this cancels
+# RestfulActions authentication logic.
 module Garage
-  # include after RestfulActions
   module NoAuthentication
     extend ActiveSupport::Concern
     include Utils
@@ -19,10 +21,13 @@ module Garage
       end
     end
 
+    # Use this method to specify requested resource_owner_id. It might be empty. Clients are not forced to
+    # send Resource-Owner-Id header.
     def resource_owner_id
       request.headers["Resource-Owner-Id"] or raise Garage::BadRequest.new('Expected Resource-Owner-Id, but empty')
     end
 
+    # Call this to know a client sends Resource-Owner-Id header or not.
     def has_resource_owner_id?
       !!request.headers["Resource-Owner-Id"]
     end
