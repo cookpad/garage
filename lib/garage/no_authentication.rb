b3836deaf80bda6f92a25184c74770b6aafee344
Taiki ONO <taiki-ono@cookpad.com>
2014-05-16 10:04:06 +0900

    Add a helper for non-authentication usage

---
+++ lib/garage/no_authentication.rb
@@ -0,0 +1,29 @@
+module Garage
+  # include after RestfulActions
+  module NoAuthentication
+    extend ActiveSupport::Concern
+
+    included do
+      use Rack::AcceptDefault
+      respond_to :json # , :msgpack
+
+      self.responder = Garage::AppResponder
+      attr_accessor :representation, :field_selector
+
+      before_filter Garage::HypermediaFilter
+      skip_before_filter :require_action_permission_crud
+
+      rescue_from Garage::HTTPError do |exception|
+        render json: { status_code: exception.status_code, error: exception.message }, status: exception.status
+      end
+    end
+
+    def resource_owner_id
+      request.headers["Resource-Owner-Id"] or raise Garage::BadRequest.new('Expected Resource-Owner-Id, but empty')
+    end
+
+    def has_resource_owner_id?
+      !!request.headers["Resource-Owner-Id"]
+    end
+  end
+end
