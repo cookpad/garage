fa782dd76e1244c577846b9e3ee9691458f74ce1
fortissimo1997 <fortissimo1997@gmail.com>
2014-12-22 23:00:01 +0900

    Garage::OptionalResponseBodyResponder#api_behavior support Rails 4.2

--- lib/garage/optional_response_body_responder.rb
+++ lib/garage/optional_response_body_responder.rb
@@ -1,14 +1,26 @@
 module Garage::OptionalResponseBodyResponder
   protected
 
-  def api_behavior(error)
+  if Rails.version.to_f < 4.2
+    def api_behavior(error)
+      api_behavior_handler || super
+    end
+  else
+    def api_behavior
+      api_behavior_handler || super
+    end
+  end
+
+  private
+
+  def api_behavior_handler
     case
     when put? && options[:put] && options[:put][:body]
       display resource, status: options[:put][:status] || :ok
     when delete? && options[:delete] && options[:delete][:body]
       display resource, status: options[:delete][:status] || :ok
     else
-      super
+      nil
     end
   end
 end
